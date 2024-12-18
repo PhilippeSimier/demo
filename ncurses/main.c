85230fcf6aba11b606d9e45fc4020ef1b96cff77
philippeSimier <philaure@wanadoo.fr>
2024-12-18 14:26:10 +0100

    add ncurses

---
+++ ncurses/main.c
@@ -0,0 +1,33 @@
+/* 
+ * File:   main.c
+ * Author: philippe Simier
+ *
+ * Created on 16 décembre 2024, 11:08
+ * 
+ * Configure le terminal pour afficher correctement les caractères Unicode.
+    Passe en mode ncurses.
+    Affiche le texte "une flamme 🔥" à la position (1, 1) du terminal.
+    Attend une interaction de l'utilisateur avant de quitter.
+    Restaure le terminal à son état normal.
+ * 
+ * compilation avec -l ncursesw
+ */
+
+#include <ncurses.h>
+#include <wchar.h>
+#include <locale.h>
+
+
+int main(int argc, char** argv) {
+
+    setlocale(LC_ALL, ""); // mettre la console en compatibilité d'affichage unicode sous linux
+    initscr(); // Initialise la structure WINDOW et autres paramètres 
+  
+
+    mvprintw(1, 1,"une flamme %lc", 0x1F525);  // on affiche l'element en x,y dans la boite
+
+    getch();
+    endwin();   // remettre le terminal dans son état initial
+    
+    return (0);
+}
