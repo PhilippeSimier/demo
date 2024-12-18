54a66271aa16b20e5a00993fe9eeb122978d600e
philippeSimier <philaure@wanadoo.fr>
2024-12-18 15:50:26 +0100

    improve getch() non bloquant

--- ncurses/main.c
+++ ncurses/main.c
@@ -22,11 +22,19 @@ int main(int argc, char** argv) {
 
     setlocale(LC_ALL, ""); // mettre la console en compatibilité d'affichage unicode sous linux
     initscr(); // Initialise la structure WINDOW et autres paramètres 
-  
+    cbreak();  // Mise en mémoire tampon de ligne désactivée.  
 
     mvprintw(1, 1,"une flamme %lc", 0x1F525);  // on affiche l'element en x,y dans la boite
-
-    getch();
+    mvprintw(23,0,"Tapez q pour quitter");
+    
+    char c;
+    do{
+        
+        c = getch();
+        
+        
+    }while(c !='q');
+    
     endwin();   // remettre le terminal dans son état initial
     
     return (0);
